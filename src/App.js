525b6f8bbcb4244b2efcb826a22252241ab100bb
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-09-13 19:47:24 -0300

    active links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import{Routes,Route} from 'react-router-dom'
 
-import Navbar from './components/Navbar.js'
+import { Navbar } from './components/Navbar.js'
 import Home from './components/Home.js'
 import About from './components/About.js'
 
