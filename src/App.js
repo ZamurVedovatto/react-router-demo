45cfe078eda9eb79d2cb58a1cbe0b8b99b644bbf
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-09-13 18:51:16 -0300

    links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import{Routes,Route} from 'react-router-dom'
 
+import Navbar from './components/Navbar.js'
 import Home from './components/Home.js'
 import About from './components/About.js'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
