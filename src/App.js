db86625917f97719187d8b46fe29a8342573fe09
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-09-15 20:56:46 -0300

    nested routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import { Navbar } from './components/Navbar.js'
 import { Home } from './components/Home.js'
 import { About } from './components/About.js'
 import { OrderSummary } from './components/OrderSummary.js'
+import { Products } from './components/Products.js'
 import { NoMatch } from './components/NoMatch.js'
+
+import { FeaturedProducts } from './components/FeaturedProducts'
+import { NewProducts } from './components/NewProducts'
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
-        <Route path="/order-summary" element={<OrderSummary />}></Route>
-        <Route path="*" element={<NoMatch />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="/products" element={<Products />}>
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
