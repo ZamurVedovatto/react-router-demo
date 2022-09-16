ef45558fc3be8e59a80bc6555dbab483d5afc7f7
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-09-15 21:15:28 -0300

    dynamic routes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { Home } from './components/Home.js'
 import { About } from './components/About.js'
 import { OrderSummary } from './components/OrderSummary.js'
 import { Products } from './components/Products.js'
-import { NoMatch } from './components/NoMatch.js'
-
 import { FeaturedProducts } from './components/FeaturedProducts'
 import { NewProducts } from './components/NewProducts'
+import { Users } from './components/Users.js'
+import { UserDetails } from './components/UserDetails.js'
+import { Admin } from './components/Admin.js'
+import { NoMatch } from './components/NoMatch.js'
+
 
 function App() {
   return (
@@ -17,11 +20,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="/order-summary" element={<OrderSummary />} />
-        <Route path="/products" element={<Products />}>
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
           <Route index element={<FeaturedProducts />} />
           <Route path="featured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
+        </Route>
+        <Route path="users" element={<Users />} >
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
         </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
---
+++ src/components/Users.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export const Users = () => {
+  return (
+    <div>
+      <NavLink to='1'>User 1</NavLink>
+      <NavLink to='2'>User 2</NavLink>
+      <NavLink to='3'>User 3</NavLink>
+      <Outlet />
+    </div>
+  )
+}
